b6010bf05ca5c07689fda1b00d64454f1554dc2e
amoghpadukone <amoghpadukone@gmail.com>
2023-08-25 18:07:18 +0530

    tailwind config file pdate + sponsor section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     extend: {
       colors: {
-        primary: "#000",
+        primary: "#0D0F16",
         secondary: "#87CEEB",
+        //color palette used
+        LushGreen: "#2FA043",
+        TerraCotta: "#EC6548",
+        SkyLavender: "#939AFF",
+        RoyalPurple: "#753FC7",
+        DeepCharcoal: "#2E2F35",
       },
     },
     fontFamily: {
