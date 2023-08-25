bcade05a832b17ff210147b598c9dbfb0ce71f24
amoghpadukone <amoghpadukone@gmail.com>
2023-08-25 18:07:34 +0530

    sponsor section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import Crew from "./components/Crew/Crew";
-import Faq from "./components/Faq/Faq";
+import {
+  default as Contact,
+  default as Contact,
+} from "./components/Contact/Contact";
+import { default as Crew, default as Crew } from "./components/Crew/Crew";
+import { default as Faq, default as Faq } from "./components/Faq/Faq";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Prizes from "./components/Prizes/Prizes";
+import Sponsers from "./components/Sponsers/Sponsers";
 import Sponsors from "./components/Sponsors/Sponsors";
 import Timeline from "./components/Timeline/Timeline";
-import Tracks from "./components/Tracks/Tracks";
 
 const App = () => {
   return (
