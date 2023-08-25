25083ccfab1bc928cdf0b92be6cf6376357c5855
SNG Viraj Reddy <vir200319@gmail.com>
2023-08-25 09:36:12 +0530

    feat(hero): Add typewriter effect, buttons, background, navbar, and responsiveness
    - Implement typewriter effect to display dynamic text
    - Introduce registration and "Join Us" buttons
    - Change background color of the Hero page (design pending)
    - Update the color of the navbar
    - Make the Hero page responsive (excluding navbar)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,8 @@ import { navs } from "./NavItems";
 
 const Navbar = () => {
   return (
-    <section className="bg-[#0B0B0B] fixed w-full z-50 backdrop-filter backdrop-blur-lg bg-opacity-30 font-roboto">
-      <div className="w-90%]  mx-auto flex items-center justify-around">
-        {/* logo */}
+    <section className="bg-[#0d0f16] fixed w-full z-50">
+      <div className="w-[80%] bg-[#0d0f16] mx-auto flex items-center justify-between">
         <div>
           <Link
             duration={100}
@@ -16,20 +15,19 @@ const Navbar = () => {
             to="hero"
           >
             <img
-              // className="__drop_shadow"
-              src="Images/reva-logo.png"
+              className="__drop_shadow"
+              src={"Images/reva-logo.png"}
               alt="logo"
               height={70}
               width={70}
             />
           </Link>
         </div>
-        {/* navlinks */}
-        <div className="flex gap-14 font-[500] ">
+        <div className="flex gap-14 font-[500]">
           {navs.map((item, i) => (
             <Link
               key={i}
-              className="  cursor-pointer  text-white text-md tracking-widest hover:text-secondary transition-all duration-200 ease-in-out"
+              className="underline-hover cursor-pointer text-white text-sm tracking-widest hover:text-secondary transition-all duration-200 ease-in-out"
               to={item.navid}
               duration={100}
               smooth={true}
