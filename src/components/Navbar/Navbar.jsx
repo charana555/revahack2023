4aee9fe7748d78c79f49ccf0fca14996546e5c59
SNG Viraj Reddy <vir200319@gmail.com>
2023-08-25 10:13:32 +0530

    feat(navbar): Added hover color animation to links
    - Added the hover color changing animation to nav bar links
    - Added blur effect to navbar
    
    blur effect - @AmoghPadukone

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,46 @@
-import "./Navbar.css";
 import { Link } from "react-scroll";
+import "./Navbar.css";
 
 import { navs } from "./NavItems";
 
 const Navbar = () => {
   return (
-    <section className="bg-[#0d0f16] fixed w-full z-50">
-      <div className="w-[80%] bg-[#0d0f16] mx-auto flex items-center justify-between">
-        <div>
-          <Link
-            duration={100}
-            smooth={true}
-            className=" cursor-pointer"
-            to="hero"
-          >
-            <img
-              className="__drop_shadow"
-              src={"Images/reva-logo.png"}
-              alt="logo"
-              height={70}
-              width={70}
-            />
-          </Link>
-        </div>
-        <div className="flex gap-14 font-[500]">
-          {navs.map((item, i) => (
+      <section className="bg-[#0B0B0B] fixed w-full z-50 backdrop-filter backdrop-blur-lg bg-opacity-30 font-roboto">
+        <div className="w-90%]  mx-auto flex items-center justify-around">
+          {/* logo */}
+          <div>
             <Link
-              key={i}
-              className="underline-hover cursor-pointer text-white text-sm tracking-widest hover:text-secondary transition-all duration-200 ease-in-out"
-              to={item.navid}
-              duration={100}
-              smooth={true}
+                duration={100}
+                smooth={true}
+                className=" cursor-pointer"
+                to="hero"
             >
-              {item.navid}
+              <img
+                  src={"Images/reva-logo.png"}
+                  alt="logo"
+                  height={70}
+                  width={70}
+              />
             </Link>
-          ))}
+          </div>
+          {/* navlinks */}
+          <div className="flex gap-14 font-[500] ">
+            {navs.map((item, i) => (
+                <Link
+                    key={i}
+                    // underline-hover class can be added
+                    className="color-transition cursor-pointer text-white text-md tracking-widest hover:text-secondary transition-all duration-200 ease-in-out"
+                    to={item.navid}
+                    duration={100}
+                    smooth={true}
+                >
+                  {item.navid}
+                </Link>
+            ))}
+          </div>
+          {/* timer */}
         </div>
-      </div>
-    </section>
+      </section>
   );
 };
 
