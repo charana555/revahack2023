8dc3d1b3fae869245b91d3bab2d2094969212831
charana555 <charanchandrashekar55@gamil.com>
2023-08-27 00:08:49 +0530

    new package: ract-icons , feat: Added contact component, feat : Added footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,63 @@
 import React from "react";
+import Socialicons from "./Socialicons";
+
+import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
 
 const Contact = () => {
   return (
     <section
       id="Contact"
-      className="flex-col text-white text-3xl w-full h-screen flex items-center justify-center"
+      className=" text-white text-3xl w-full  px-[5%] py-5 md:px-[10%] md:py-10 gap-5 grid md:grid-cols-2 grid-cols-1"
     >
-      <p>Contact</p>
-      <p className=" text-4xl text-secondary drop-shadow-lg shadow-secondary ">
-        Comming Soon . . .
-      </p>
+      <div className=" w-full h-[400px] px-2 md:px-[22%]">
+        <iframe
+          className="w-full h-full"
+          title="map"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3885.7291172872892!2d77.63310401482352!3d13.116339290761024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae19721a651fd3%3A0xdee225fe28f600f6!2sREVA%20University!5e0!3m2!1sen!2sin!4v1665244381917!5m2!1sen!2sin"
+          style={{ border: "0" }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <div className=" w-full h-[10vh]  md:px-[10%]">
+        <h6 className="text-lg px-2 tracking-wider">Follow Us</h6>
+        <Socialicons />
+
+        <div className=" flex flex-col gap-2 mb-10">
+          <a
+            href="#"
+            className="text-lg px-2 tracking-wider block font-extralight"
+          >
+            Terms and Condition
+          </a>
+          <a
+            href="#"
+            className="text-lg px-2 tracking-wider block font-extralight"
+          >
+            Privacy Policy
+          </a>
+          <a
+            href="#"
+            className="text-lg px-2 tracking-wider block font-extralight"
+          >
+            Code of Conduct
+          </a>
+        </div>
+
+        <div>
+          <h6 className="text-lg px-4 tracking-wider">Contact Us</h6>
+
+          <div className=" flex text-lg items-center gap-2 tracking-wider">
+            <AiOutlinePhone className="text-2xl font-extrabold" /> +91
+            9999999999
+          </div>
+          <div className=" flex text-lg items-center gap-2 tracking-wider">
+            <AiOutlineMail className="text-2xl font-extrabold" />{" "}
+            revahack@gmail.com
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section className=" bg-black text-white flex justify-between w-full px-[10%] p-3 tracking-wider">
+    <section className=" bg-black flex-wrap text-white flex justify-between w-full px-[10%] p-3 tracking-wider">
       <p> CopyRight &copy; 2023 All Right Reserved </p>
       <p>|</p>
       <p>
