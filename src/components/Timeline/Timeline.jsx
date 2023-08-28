d4e59e1a33e4b2abd1d82a81b218fd34645d9412
Dhyuti Tewani <55708942+Dhyuti@users.noreply.github.com>
2023-08-28 10:57:22 +0530

    create base timeline

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Timeline.css";
+import "./Timeline2.js";
 
 const Timeline = () => {
   return (
@@ -7,9 +9,45 @@ const Timeline = () => {
       className="flex-col text-white text-3xl w-full h-screen flex items-center justify-center"
     >
       <p>Timeline</p>
-      <p className=" text-4xl text-secondary drop-shadow-lg shadow-secondary ">
-        Comming Soon . . .
-      </p>
+
+      <div class="custom-section mb-[20%]">
+        <div id="progressbar" class="timeline-main">
+          <div class="progress-wrap">
+            <div class="pogress"></div>
+          </div>
+          <ul>
+            
+            <li class="adding">
+              <div class="custom-box">
+                <h3>Contrary to popular</h3>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here'.</p>
+              </div>
+            </li>
+            
+            <li class="">
+              <div class="custom-box">
+                <h3>Contrary to popular</h3>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here'.</p>
+              </div>
+            </li>
+            
+            <li class="">
+              <div class="custom-box">
+                <h3>Contrary to popular</h3>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here'.</p>
+              </div>
+            </li>
+            
+            <li class="">
+              <div class="custom-box">
+                <h3>Contrary to popular</h3>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here'.</p>
+              </div>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
