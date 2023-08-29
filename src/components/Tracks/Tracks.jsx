a370c12c7728afc1c39ebc2f776638a998cc3a07
charana555 <charanchandrashekar55@gamil.com>
2023-08-29 19:02:53 +0530

    FAQ's data updated

--- src/components/Tracks/Tracks.jsx
+++ src/components/Tracks/Tracks.jsx
@@ -3,9 +3,8 @@ import VanillaTilt from "vanilla-tilt";
 import "./Tracks.css";
 
 const Tracks = () => {
-  
   useEffect(() => {
-    VanillaTilt.init(document.querySelectorAll(".card"),{
+    VanillaTilt.init(document.querySelectorAll(".card"), {
       max: 3,
       speed: 400,
       glare: true,
@@ -21,92 +20,140 @@ const Tracks = () => {
     });
   }, []);
 
-
-
   return (
     <section
       id="Tracks"
-      className="mt-[10%] p-10 bg-primary flex-col text-white flex items-center justify-center">
+      className="mt-[10%] p-10 bg-primary flex-col text-white flex items-center justify-center"
+    >
       <p className="text-8xl text-secondary">Tracks</p>
       {/* card1 */}
-      <div 
-        id="container" 
-        className="flex flex-row flex-wrap items-center justify-center relative max-w-7xl z-1">
+      <div
+        id="container"
+        className="flex flex-row flex-wrap items-center justify-center relative max-w-7xl z-1"
+      >
         <div class="card">
-          <div id="img"
-            className="absolute top-0 left-16 right-0 bottom-40 bg-cover bg-center bg-no-repeat">
-              <img src="./public/images/GenAI.svg" alt="genai" className="w-4/6 h-full" />
+          <div
+            id="img"
+            className="absolute top-0 left-16 right-0 bottom-40 bg-cover bg-center bg-no-repeat"
+          >
+            <img
+              src="./public/images/GenAI.svg"
+              alt="genai"
+              className="w-4/6 h-full"
+            />
           </div>
-          <div 
-            id="head" 
-            className="absolute top-52 text-2xl">Gen AI</div>
-          <div 
-            id="content" 
-            className="absolute top-60 left-5 right-4 text-center items-center z-1">
-            <p>Ctrl+C Ctrl+V to the Future: Hacking Tomorrow's Universe with AI</p>
+          <div id="head" className="absolute top-52 text-2xl">
+            Gen AI
+          </div>
+          <div
+            id="content"
+            className="absolute top-60 left-5 right-4 text-center items-center z-1"
+          >
+            <p>
+              Ctrl+C Ctrl+V to the Future: Hacking Tomorrow's Universe with AI
+            </p>
           </div>
         </div>
         {/* card2 */}
         <div class="card">
-        <div id="img"
-            className="absolute top-0 left-0 right-0 bottom-40 bg-cover bg-center bg-no-repeat">
-              <img src="./public/images/blockchain.svg" alt="blockchain" className="w-full h-full" />
+          <div
+            id="img"
+            className="absolute top-0 left-0 right-0 bottom-40 bg-cover bg-center bg-no-repeat"
+          >
+            <img
+              src="./public/images/blockchain.svg"
+              alt="blockchain"
+              className="w-full h-full"
+            />
           </div>
-          <div 
-            id="head" 
-            className="absolute top-52 text-2xl">Block Chain</div>
-          <div 
-            id="content" 
-            className="absolute top-60 left-5 right-4 text-center items-center z-1">
-            <p>Building the Future on Immutable Blocks: Hack the Potential of Blockchain</p>
+          <div id="head" className="absolute top-52 text-2xl">
+            Block Chain
+          </div>
+          <div
+            id="content"
+            className="absolute top-60 left-5 right-4 text-center items-center z-1"
+          >
+            <p>
+              Building the Future on Immutable Blocks: Hack the Potential of
+              Blockchain
+            </p>
           </div>
         </div>
         {/* card3 */}
         <div class="card">
-        <div id="img"
-            className="absolute top-0 left-16 right-0 bottom-40 bg-cover bg-center bg-no-repeat">
-              <img src="./public/images/ARVR.svg" alt="arvr" className="w-4/6 h-full" />
+          <div
+            id="img"
+            className="absolute top-0 left-16 right-0 bottom-40 bg-cover bg-center bg-no-repeat"
+          >
+            <img
+              src="./public/images/ARVR.svg"
+              alt="arvr"
+              className="w-4/6 h-full"
+            />
           </div>
-          <div 
-            id="head" 
-            className="absolute top-52 text-2xl">AR VR</div>
-          <div 
-            id="content" 
-            className="absolute top-60 left-5 right-4 text-center items-center z-1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vulputate pellentesque massa quis tempus.</p>
+          <div id="head" className="absolute top-52 text-2xl">
+            AR VR
+          </div>
+          <div
+            id="content"
+            className="absolute top-60 left-5 right-4 text-center items-center z-1"
+          >
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              vulputate pellentesque massa quis tempus.
+            </p>
           </div>
         </div>
         {/* card4 */}
         <div class="card">
-        <div id="img"
-            className="absolute top-0 left-16 right-0 bottom-40 bg-cover bg-center bg-no-repeat">
-              <img src="./public/images/CloudIOT.svg" alt="cloudiot" className="w-4/6 h-full" />
+          <div
+            id="img"
+            className="absolute top-0 left-16 right-0 bottom-40 bg-cover bg-center bg-no-repeat"
+          >
+            <img
+              src="./public/images/CloudIOT.svg"
+              alt="cloudiot"
+              className="w-4/6 h-full"
+            />
           </div>
-          <div 
-            id="head" 
-            className="absolute top-52 text-2xl">Cloud IOT</div>
-          <div 
-            id="content" 
-            className="absolute top-60 left-5 right-4 text-center items-center z-1">
-            <p>Code in the Cloud, Control the World: Building Smart Solutions with IoT and Cloud</p>
+          <div id="head" className="absolute top-52 text-2xl">
+            Cloud IOT
+          </div>
+          <div
+            id="content"
+            className="absolute top-60 left-5 right-4 text-center items-center z-1"
+          >
+            <p>
+              Code in the Cloud, Control the World: Building Smart Solutions
+              with IoT and Cloud
+            </p>
           </div>
         </div>
         {/* card5 */}
         <div class="card">
-        <div id="img"
-            className="absolute top-0 left-0 right-0 bottom-40 bg-cover bg-center bg-no-repeat">
-              <img src="./public/images/CyberSecurity.svg" alt="cybersecurity" className="w-full h-full" />
+          <div
+            id="img"
+            className="absolute top-0 left-0 right-0 bottom-40 bg-cover bg-center bg-no-repeat"
+          >
+            <img
+              src="./public/images/CyberSecurity.svg"
+              alt="cybersecurity"
+              className="w-full h-full"
+            />
           </div>
-          <div 
-            id="head" 
-            className="absolute top-52 text-2xl">Cybersecurity</div>
-          <div 
-            id="content" 
-            className="absolute top-60 left-5 right-4 text-center items-center z-1">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vulputate pellentesque massa quis tempus.</p>
+          <div id="head" className="absolute top-52 text-2xl">
+            Cybersecurity
+          </div>
+          <div
+            id="content"
+            className="absolute top-60 left-5 right-4 text-center items-center z-1"
+          >
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              vulputate pellentesque massa quis tempus.
+            </p>
           </div>
         </div>
-
       </div>
     </section>
   );
--- src/data/faqData.js
+++ src/data/faqData.js
@@ -1,82 +1,78 @@
 export const data = [
   {
-    faq: "When will REVA HACK 2022 take place?",
-    ans: "REVA HACK 2022 is scheduled for 11th-13th of November over 48 hours accompanied by various pre-events like learnathons, workshops, etc.",
+    faq: "When will REVA HACK 2023 take place?",
+    ans: "REVA HACK 2023 is scheduled for 2nd-4th of November over 48 hours accompanied by various pre-events like learnathons, workshops, etc.",
   },
   {
     faq: "Is it a remote or an on-site Hackathon?",
-    ans: "This time REVA HACK goes the best of both worlds, it's a Hybrid Hackathon. Hackers can join us either virtually or physically (on campus), whatever suits them the best.",
+    ans: "This year, we are only hosting an on-campus event, meaning it will be conducted in person rather than online.",
   },
   {
-    faq: "What is Learnathon? What happens at a Learnathon?",
-    ans: "Learnathon is a pre-event talk and workshop session dedicated to increasing the awareness of hackathons and similar tech events among the attendees. The event aims to ignite the spirit of hackathons among students.",
+    faq: "What is the maximum and minimum number of participants allowed in a group?",
+    ans: "A group must have at least one participant and cannot exceed five people. It is required to have at least one female participant in each group.",
   },
   {
-    faq: "Are there any specific programming languages or domains discussed in Learnathon?",
-    ans: "Learnathon is a mix of various tech demonstrations and workshops not particularly focused on a single domain/language. The exact schedule of the vent is to be released soon.",
-  },
-  {
-    faq: "Does REVA HACK 2022 have any restrictions on the programming language to be used?",
-    ans: "Nope, you are free to use whatever tech stalk/programming language you please. From Haskell to Holy C, whatever sails your boat.",
-  },
-  {
-    faq: "Who can apply for REVA HACK 2022?",
-    ans: "Students & working professionals from all disciplines are welcome to apply for the event.",
-  },
-  {
-    faq: "Are meals and accommodation provided at the venue?",
-    ans: "If approved to attend the event on-site, the participants will be provided food and accommodation for the duration of the event.",
-  },
-  {
-    faq: "What kind of skill set is required for REVA HACK 2022?",
-    ans: "There is no such skill required as a prerequisite for the hackathon. Although we do hope to see the skill of innovation among the participants.",
-  },
-  {
-    faq: "How does one avail accommodation facilities?",
-    ans: "Only approved participants will be provided accommodation on-site during the event. The details of accommodation will be informed to the participants after all the paperwork is verified at the venue.",
-  },
-  {
-    faq: "Will I miss out on anything if I join the event virtually?",
-    ans: "REVA HACK 2022 maintains a level playing ground for Virtual and in-person attendees. But engagement events and stress busters will mostly be available only to the in-person attendees.",
+    faq: "Who can apply for REVA HACK 2023?",
+    ans: "Students from all colleges and universities pursuing any undergraduate or post-graduate course are welcome to apply for the event.",
   },
   {
     faq: "What do I pay to register for the event?",
-    ans: "0, Nill, Nought, Cero, Nada. REVA HACK 2022 will be a free-to-attend event for all participants.",
+    ans: "0, Nill, Nought, Cero, Nada. REVA HACK 2023 has no registration fee for all participants apart from rupees 300 paid as a security deposit which will be refunded after the event unless any property damage is caused.",
+  },
+  {
+    faq: "How does one avail accommodation facilities?",
+    ans: "Yes, approved participants will be provided accommodation and meals on-site during the event. The details of accommodation will be informed to the participants after all the paperwork is verified at the venue.",
+  },
+  {
+    faq: "Are travel expenses included too?",
+    ans: "No, the participants are responsible for covering their own travel expenses.",
+  },
+  {
+    faq: "Does REVA HACK 2023 have any restrictions on the programming language to be used?",
+    ans: "Nope, you are free to use whatever tech stack/programming language you please. From Haskell to Holy C, whatever sails your boat.",
+  },
+  {
+    faq: "What kind of skill set is required for REVA HACK 2023?",
+    ans: "There is no such skill required as a prerequisite for the hackathon. Although we do hope to see the skill of innovation among the participants.",
+  },
+  {
+    faq: "Will free WiFi be provided?",
+    ans: "Yes, Wi-Fi access will be shared with all participants.",
+  },
+  {
+    faq: "Is it possible for us to utilize open-source resources to replicate the project code?",
+    ans: "No, every project will be checked for plagiarism and copyright violations. Groups caught will be disqualified.",
   },
   {
     faq: "How do I submit my project for the Hackathon?",
-    ans: "We will take submissions through the Devfolio portal in accordance with their code of conduct. Further details on the submission and evaluation phases will be e-mailed to the participants.",
-  },
-  {
-    faq: "Do you need to have your project fully functional for submission?",
-    ans: "You can always submit your project at whatever stage you want but the progress made during the hackathon is a key criterion for evaluation.",
-  },
-  {
-    faq: "Will you provide any computers or Software to work with?",
-    ans: "We prefer participants to carry their computers and tools for the event. If required, arrangements for computer facilities can be made on the campus. As for software tool availability, that'll be posted shortly.",
-  },
-  {
-    faq: "Does one have to be present online for the entire duration of the event?",
-    ans: "The participants have to be reachable for the entire duration of the event. Progress evaluation and elevator pitch events will be held at fixed durations throughout the hackathon, they will also be considered for the final grading. The schedule for the same will be available shortly.",
+    ans: "We will take submissions through the Devfolio portal in accordance with their code of conduct. Further details on the submission and evaluation phases will be informed to the participants after all the paperwork is verified at the venue.",
   },
   {
     faq: "Are there any restrictions on the use of pre-built libraries and packages?",
-    ans: "Technically no, you can use as many packages, libraries, and, modules as you want. But your contribution to your project will outweigh everything else during the final evaluation.",
-  },
-  {
-    faq: "Who will own the IP ( Intellectual Property) of the projects made during REVA HACK 2022?",
-    ans: "In short YOU. Every project submitted during REVA HACK 2022 needs to come from an Open Source repo that's publicly accessible. The organizers of the event in no manner or form shall have any stake in the projects made during the event.",
+    ans: "Technically no, you can use as many packages, libraries, and modules as you want. But your contribution to your project will outweigh everything else during the final evaluation.",
   },
   {
     faq: "Do I need to demo my project during the event?",
     ans: "You will be required to demonstrate your project and share your work progress several times during the event. These demonstrations will be considered for grading and Evaluation.",
   },
   {
-    faq: "Is it ok to just show a prototype of my project?",
-    ans: "You can always submit a working prototype of your idea. It'll be evaluated based on progress made on the project during the event.",
+    faq: "Will you provide any computers or Software to work with?",
+    ans: "We prefer participants to carry their own devices and tools for the event. If required, arrangements for computer facilities can be made on the campus. As for software tool availability, that'll be posted shortly.",
+  },
+  {
+    faq: "Who will own the IP (Intellectual Property) of the projects made during REVA HACK 2022?",
+    ans: "In short YOU, for REVA HACK 2023, all project submissions must be from publicly accessible Open Source repositories. The event organizers will not have any stake or involvement in the projects created during the event.",
+  },
+  {
+    faq: "Can a person be a part of two groups?",
+    ans: "No.",
+  },
+  {
+    faq: "Will any goodies be provided to participants?",
+    ans: "Participants will be given free goodies such as t-shirts, stickers, swags, bags and more.",
   },
   {
     faq: "Are you sure I won't get bored or lose motivation?",
-    ans: "We've got you covered fam, we have many events lined up just to keep you engaged and invested in the event. From icebreakers to stress Busters, bored says goodbye at REVA HACK",
+    ans: "We've got you covered fam, we have many events lined up just to keep you engaged and invested in the event. From icebreakers to stress Busters, boredom says goodbye at REVA HACK.",
   },
 ];
