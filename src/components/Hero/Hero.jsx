0b34c43cafddc2e6cc0980fcffec17be6a65a2ab
SNG Viraj Reddy <vir200319@gmail.com>
2023-09-02 22:39:22 +0530

    Update Hero page
    - Update typewriter text
    - Update nav bar icon
    - Increase button sizes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,29 +8,37 @@ const Hero = () => {
       <section id="Hero">
         <div className="hero min-h-screen flex items-center justify-center text-white">
           <div>
-            <div className="my-32">
+            <div className="my-24">
+              <div className='flex justify-center md:space-x-4 mb-5 font-semibold'>
+                <p className='hidden md:block text-[2px] md:text-4xl text-[#6D5BF6]'>SPONSORED BY</p>
+                <img src={'Images/ieeelogo.png'} alt='ieee logo' className='h-10 w-24 md:h-58 md:w-126' />
+              </div>
+
+
+
+
+
               <div className="text-center mb-4">
-                <h1 className="text-4xl font-bold md:text-6xl lg:text-8xl">REVA HACK {'</>'} 2023</h1>
+                <h1 className="text-4xl font-bold md:text-6xl lg:text-[105px]">REVA HACK {'</>'} 2023</h1>
               </div>
 
               {/* The typewriter text */}
               <div className="text-center">
-                <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl relative mb-4 text-DeepCharcoal font-fira font-medium">
+                <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl relative mb-4 text-[#0CCA4A] font-roboto font-[300]">
                   {currentText}
                   <span className="cursor" style={cursorStyle}></span>
                 </p>
               </div>
             </div>
-
             {/* Responsive buttons */}
-            <div className="flex flex-col items-center space-y-4 sm:flex-row sm:space-x-32 sm:space-y-0 justify-center">
-              <button className="bg-[#304ce4] hover:shadow-lg px-12 py-4 rounded-lg text-white text-xl md:text-3xl  flex items-center">
+            <div className="flex flex-col items-center space-y-4 sm:flex-row sm:space-x-32 sm:space-y-0 justify-center font-roboto">
+              <button className="bg-[#304ce4] hover:shadow-lg px-12 py-4 rounded-[16px] text-white justify-center text-xl md:text-5xl flex items-center md:h-[100px] md:w-[420px]">
                 <span className="mr-4">Register</span>
-                <img src={"Images/devfolio.png"} alt="devfolio logo" className="w-6 h-6 ml-3" />
+                <img src={"Images/devfolio.png"} alt="devfolio logo" className="w-6 h-6 md:w-9 md:h-9 ml-3" />
               </button>
-              <button className="bg-[#5c54f4] hover:shadow-lg px-12 py-4 rounded-lg text-white text-xl md:text-3xl flex items-center">
+              <button className="bg-[#5c54f4] hover:shadow-lg px-12 py-4 rounded-[16px] text-white text-xl md:text-5xl flex items-center justify-center md:h-[100px] md:w-[420px]">
                 <span className="mr-4">Join Us</span>
-                <img src={"Images/discord.png"} alt="discord logo" className="w-8 h-6 ml-3" />
+                <img src={"Images/discord.png"} alt="discord logo" className="w-8 h-6 md:w-11 md:h-8 ml-3" />
               </button>
             </div>
           </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,6 @@ import { navs } from "./NavItems";
 import "./Navbar.css";
 
 const Navbar = () => {
-
   const [menubarOpen, setMenubarOpen] = useState(false);
 
   const toggleMenubar = () => {
@@ -18,74 +17,67 @@ const Navbar = () => {
     setMenubarOpen(false);
   };
 
-  const menuClass = menubarOpen ? "dropdown-menu show " : "dropdown-menu";
+  const menuClass = menubarOpen ? "dropdown-menu show" : "dropdown-menu";
   const menuItemsClass = menubarOpen ? "block" : "hidden";
 
   return (
-    <nav
-      className={`bg-primary fixed w-full z-50 backdrop-filter backdrop-blur-lg bg-opacity-30`}
-    >
-      <div className="container mx-auto flex flex-wrap items-center justify-between px-4 font-roboto">
-        {/* Logo */}
-        <div>
-          <Link
-            duration={100}
-            smooth={true}
-            className="cursor-pointer"
-            to="hero"
-          >
-            <img
-              src={"Images/reva-logo.png"}
-              alt="logo"
-              height={70}
-              width={70}
-            />
-          </Link>
-        </div>
-        {/* Navlinks for Desktop */}
-        <div
-          className={`hidden md:flex md:gap-14 font-semibold  ${
-            menubarOpen ? "hidden" : ""
-          }`}
-        >
-          {navs.map((item, i) => (
-            <Link
-              key={i}
-              className="underline-hover cursor-pointer text-white text-md tracking-widest hover:text-secondary transition-all duration-200 ease-in-out"
-              to={item.navid}
-              duration={100}
-              smooth={true}
-            >
-              {item.navid}
+      <nav className={`bg-primary fixed w-full z-50 backdrop-filter backdrop-blur-lg bg-opacity-90`}>
+        <div className="container mx-auto flex flex-wrap items-center justify-between px-4 font-roboto">
+          {/* Logo */}
+          <div>
+            <Link duration={100} smooth={true} className="cursor-pointer" to="hero">
+              <img src={"Images/reva-logo.png"} alt="logo" height={70} width={70} />
             </Link>
-          ))}
+          </div>
+          {/* Navlinks for Desktop */}
+          <div className={`hidden md:flex md:gap-14 font-semibold ${menubarOpen ? "hidden" : ""}`}>
+            {navs.map((item, i) => (
+                <Link
+                    key={i}
+                    className="underline-hover cursor-pointer text-white text-md tracking-widest hover:text-secondary transition-all duration-200 ease-in-out"
+                    to={item.navid}
+                    duration={100}
+                    smooth={true}
+                >
+                  {item.navid}
+                </Link>
+            ))}
+          </div>
+          {/* Hamburger Menu */}
+          <div className="md:hidden">
+            <button className="text-white cursor-pointer menu-button" onClick={toggleMenubar}>
+              {menubarOpen ? (
+                  /* "X" Icon with transition effect */
+                  <svg width="24" height="24" fill="none" stroke="#FFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="transition-transform transform rotate-90">
+                    <path d="M6 18L18 6M6 6l12 12"></path>
+                  </svg>
+              ) : (
+                  /* Hamburger icon with transition effect */
+                  <svg width="30" height="20" viewBox="0 0 30 20" xmlns="http://www.w3.org/2000/svg" className="transition-transform">
+                    <rect width="25" height="2" rx="1" fill="#FFF" />
+                    <rect x="9" y="8" width="15" height="2" rx="1" fill="#FFF" />
+                    <rect y="16" width="25" height="2" rx="1" fill="#FFF" />
+                  </svg>
+              )}
+            </button>
+          </div>
+          {/* Menubar */}
+          <div className={`md:hidden w-full my-2  ${menuClass} mobile-nav`}>
+            {navs.map((item, i) => (
+                <Link
+                    key={i}
+                    className={`cursor-pointer py-4 transition-all duration-200 ease-in-out hover:text-[#81D9D2FF] ${menuItemsClass} menu-item`}
+                    to={item.navid}
+                    duration={100}
+                    smooth={true}
+                    onClick={closeMenubar}
+                >
+                  {item.navid}
+                </Link>
+            ))}
+          </div>
         </div>
-        {/* Hamburger Menu */}
-        <div className="md:hidden">
-          <button
-            className="text-white cursor-pointer menu-button"
-            onClick={toggleMenubar}
-          >
-            ☰{/* Hamburger icon */}
-          </button>
-        </div>
-        {/* Menubar */}
-        <div className={`md:hidden w-full my-2  ${menuClass} mobile-nav`}>
-          {navs.map((item, i) => (
-            <Link
-              key={i}
-              className={`cursor-pointer py-4 transition-all duration-200 ease-in-out hover:text-[#81D9D2FF] ${menuItemsClass} menu-item`}
-              to={item.navid}
-              duration={100}
-              smooth={true}
-              onClick={closeMenubar}
-            >
-              {item.navid}
-            </Link>
-          ))}
-        </div>
-      </div>
-    </nav>
+      </nav>
   );
 };
 
