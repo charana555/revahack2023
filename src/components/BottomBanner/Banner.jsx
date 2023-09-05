14aed2234b5f34a7c4dfbc9840980b101daddbc1
Amogh <amoghpadukone@gmail.com>
2023-09-05 15:52:37 +0530

    glimpse section improvement

--- src/components/BottomBanner/Banner.jsx
+++ src/components/BottomBanner/Banner.jsx
@@ -3,18 +3,18 @@ import "./Banner.css";
 
 const Banner = (props) => {
   return (
-    <section className="flex flex-col text-white text-center text-3xl items-center justify-center pixel-bg my-20">
+    <section className="  flex flex-col text-white text-center text-3xl items-center justify-center pixel-bg mb-20">
       {/* <div id="img" className="h-auto justify-center items-center">
         <img src="./Images/pixel.svg" alt="pixel" className="absolute w-[100%] h-[50%]" />
       </div> */}
-          <div className="flex flex-col justify-center items-center mx-auto glassmorphic-card w-[60%] md:max-w-[40%] py-8 text-center">
-            <div className="main text-white font-bold md:text-6xl text-5xl xs:text-4xl">
-              <span className="text-LushGreen">REVA HACK</span> 2023
-            </div>
-            <div className="sub font-light text-[#7C8590] md:text-2xl text-xl">
-              Technology beyong Imagination
-            </div>
-          </div>
+      <div className="flex flex-col justify-center items-center mx-auto glassmorphic-card w-[90%] md:max-w-[60%] m-auto py-8 text-center">
+        <div className="main text-white font-bold md:text-5xl text-4xl xs:text-4xl">
+          <span className="text-LushGreen">REVA HACK</span> 2023
+        </div>
+        <div className="sub font-light text-[#7C8590] md:text-2xl text-xl">
+          Technology beyong Imagination
+        </div>
+      </div>
     </section>
   );
 };
