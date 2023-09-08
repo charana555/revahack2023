0caec67564c37c79fb2641a8b6a5cbede42116af
Amogh <amoghpadukone@gmail.com>
2023-09-08 16:47:25 +0530

    responsiveness fix in hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -82,7 +82,10 @@ const Hero = () => {
 
             {/* The typewriter text */}
             <div className="text-center">
-              <p className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-4xl relative mb-4 text-[#0CCA4A] font-fira font-[300]">
+              <p
+                id="test"
+                className="text-2xl  md:text-4xl  relative mb-4 text-[#0CCA4A] font-fira font-[300]"
+              >
                 {currentText}
                 <span className="cursor" style={cursorStyle}></span>
               </p>
