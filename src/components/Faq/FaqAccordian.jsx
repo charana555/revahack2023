a0ebe2b43991e79ab66b4f735976bdf4d0799209
Amogh <amoghpadukone@gmail.com>
2023-09-09 18:58:22 +0530

    faq section color change

--- src/components/Faq/FaqAccordian.jsx
+++ src/components/Faq/FaqAccordian.jsx
@@ -1,19 +1,19 @@
-import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 import { Collapse } from "react-collapse";
+import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 
 const FaqAccordian = ({ open, toggle, faq, ans, index }) => {
   return (
     <div>
       <div
         className={` ${!open ? "rounded-lg" : "rounded-t-lg"} ${
-          index % 4 < 2 ? "bg-NeonGreen" : "bg-NeonViolet"
-        } flex justify-center items-center cursor-pointer text-lg px-4 py-2.5 rounded-t-lg `}
+          index % 4 < 2 ? "bg-[#23282E]" : "bg-[#23282E]"
+        } flex justify-center items-center cursor-pointer text-lg px-5 py-4  rounded-t-lg  `}
         onClick={toggle}
       >
         <p className="flex-1 text-sm text-center  font-fira">
           {open ? faq : faq.length > 50 ? `${faq.substring(0, 50)}...` : faq}
         </p>
-        <div className=" rounded-full p-1 text-[8px] text-black border border-black ">
+        <div className=" rounded-full p-1 text-[8px] text-white border border-white ">
           {open ? <AiOutlineCaretUp /> : <AiOutlineCaretDown />}
         </div>
       </div>
@@ -21,8 +21,8 @@ const FaqAccordian = ({ open, toggle, faq, ans, index }) => {
       <Collapse isOpened={open}>
         <div
           className={` ${
-            index % 4 < 2 ? "bg-NeonGreen" : "bg-NeonViolet"
-          } text-sm px-4 py-3 tracking-wider rounded-b-lg font-fira text-center   `}
+            index % 4 < 2 ? "bg-[#23282E]" : "bg-[#23282E]"
+          } text-sm px-5 py-4 tracking-wider rounded-b-lg font-fira text-center   `}
         >
           {ans}
         </div>
