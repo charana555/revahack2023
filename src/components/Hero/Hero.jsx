4615f1aa928c15f3c2bd32396b3ea48119f676b3
SNG Viraj Reddy <vir200319@gmail.com>
2023-09-09 15:59:16 +0530

    Design update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,43 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js"; // Import the Typed.js library
 import GLOBE from "vanta/src/vanta.globe";
 import RINGS from "vanta/src/vanta.rings";
 import WAVES from "vanta/src/vanta.waves";
 import "./Hero.css";
 import useHeroTypingEffect from "./HeroLogic.js";
 import Timer from "./Timer.jsx";
+import './neon.css'
 
 const Hero = () => {
-  const { currentText, cursorStyle } = useHeroTypingEffect();
-  React.useEffect(() => {
+  const { cursorStyle } = useHeroTypingEffect();
+
+  const typedRef = useRef(null); // Create a ref for the Typed instance
+
+  useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://apply.devfolio.co/v2/sdk.js";
     script.async = true;
     script.defer = true;
     document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  useEffect(() => {
-    // GLOBE({
-    //   el: "#vanta",
-    //   mouseControls: true,
-    //   touchControls: true,
-    //   gyroControls: true,
-    //   minHeight: 200.0,
-    //   minWidth: 200.0,
-    //   scale: 0.8,
-    //   scaleMobile: 0.5,
-    //   // color: 0x2fa043,
-    //   // color: 0x008d2f,
-    //   // color: 0xEC6548,
-    //   color: 0x6d5bf6,
-    //   // color2: 0xd2d2d2,
-    //   color2: 0x0d0f16,
-    //   size: 1.5,
-    //   backgroundColor: 0x0d0f16,
-    // });
 
     RINGS({
       el: "#vanta",
@@ -51,80 +32,94 @@ const Hero = () => {
       backgroundColor: 0x0d0f16,
       color: 0xff0000,
     });
-    //
-    // WAVES({
-    //   el: "#vanta",
-    //   mouseControls: true,
-    //   touchControls: true,
-    //   gyroControls: false,
-    //   minHeight: 200.0,
-    //   minWidth: 200.0,
-    //   scale: 1.0,
-    //   scaleMobile: 1.0,
-    //   shininess: 17.0,
-    //   waveHeight: 19.0,
-    //   waveSpeed: 0.9,
-    //   zoom: 0.65,
-    // });
+
+// Initialize Typed.js inside this useEffect
+    typedRef.current = new Typed('.typed-text', {
+      strings: ["Let the hack begin;", "Innovation for tomorrow"],
+      typeSpeed: 50,
+      backSpeed: 25,
+      startDelay: 1000,
+      backDelay: 1000,
+      loop: true,
+      showCursor: true,
+      cursorChar: '<span class="cursor-grey">|</span>', // Customize the cursor with a CSS class
+      onComplete: (self) => {
+        self.cursor.remove();
+      },
+    });
+
+
+    return () => {
+      if (typedRef.current) {
+        typedRef.current.destroy();
+      }
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
-    <section id="Hero">
-      <div id="vanta"> </div>
-      <div className="hero min-h-screen flex flex-col items-center justify-center text-white">
-        <div className="text-center mt-20">
-          <div className="my-24">
-            <div className="text-center mb-6">
-              <h1 className="text-4xl md:text-5xl lg:text-8xl xl:text-8xl font-semibold">
-                REVA HACK {"</>"} 2023
-              </h1>
-            </div>
+      <section id="Hero">
+        <div id="vanta"> </div>
+        <div className="hero min-h-screen flex flex-col items-center justify-center text-white">
+          <div className="text-center mt-20">
+            <div className="my-24">
 
-            {/* The typewriter text */}
-            <div className="text-center">
-              <p className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-4xl relative mb-4 text-[#0CCA4A] font-fira font-[300]">
-                {currentText}
-                <span className="cursor" style={cursorStyle}></span>
-              </p>
-            </div>
-            <Timer />
-            <div className="flex flex-col items-center space-y-4 md:flex-row md:space-x-20 md:space-y-0 justify-center mt-6">
-              <div
-                className="apply-button"
-                data-hackathon-slug="reva-hack-1"
-                data-button-theme="light"
-                style={{ height: "44px", width: "312px" }}
-              >
-                <button>
-                  <span className="mr-4">Register</span>
-                  <img
-                    src={"Images/devfolio.png"}
-                    alt="devfolio logo"
-                    className="w-6 h-6 md:w-9 md:h-9 ml-3"
-                  />
-                </button>
+              <div className="text-center mb-2 text-sm sm:text-2xl md:text-3xl lg:text-4xl neon">
+                36 hours offline hackathon
               </div>
-              <button
-                className="hover:shadow-lg px-12 rounded-[3px] text-white text-2xl flex items-center justify-center h-[44px] w-[312px]"
-                style={{ backgroundColor: "#5865F2", border: "none" }}
-              >
-                <img
-                  src={"Images/discord.png"}
-                  alt="discord logo"
-                  className="w-8 h-6 mr-3"
-                />
-                <span
-                  className="mr-4 font-[530]"
-                  style={{ fontFamily: "Nunito Sans, sans-serif" }}
+
+              <div className="text-center mb-6">
+                <h1 className="text-4xl md:text-5xl lg:text-8xl xl:text-8xl font-semibold" data-text='U'>
+                  {/*flicker-slow neon-white*/}
+                  <span className="">REVA HACK </span><span className="">{"</>"}</span> <span className="neon-white">2023</span>
+                </h1>
+              </div>
+
+              {/* The typewriter text */}
+              <div className="text-center text-2xl text-glow md:text-4xl lg:text-4xl xl:text-4xl relative mb-4 font-fira font-[300]">
+                <span className="typed-text text-[#0CCA4A]"></span> {/* Typed.js will type the text here */}
+              </div>
+              <div className='text-[#BCBCBC]'>
+                <Timer />
+              </div>
+
+              <div className="flex flex-col items-center space-y-4 md:flex-row md:space-x-20 md:space-y-0 justify-center mt-6">
+                <div
+                    className="apply-button"
+                    data-hackathon-slug="reva-hack-1"
+                    data-button-theme="light"
+                    style={{ height: "44px", width: "312px" }}
                 >
+                  <button>
+                    <span className="mr-4">Register</span>
+                    <img
+                        src={"Images/devfolio.png"}
+                        alt="devfolio logo"
+                        className="w-6 h-6 md:w-9 md:h-9 ml-3"
+                    />
+                  </button>
+                </div>
+                <button
+                    className="hover:shadow-lg px-12 rounded-[3px] text-white text-2xl flex items-center justify-center h-[44px] w-[312px]"
+                    style={{ backgroundColor: "#5865F2", border: "none" }}
+                >
+                  <img
+                      src={"Images/discord.png"}
+                      alt="discord logo"
+                      className="w-8 h-6 mr-3"
+                  />
+                  <span
+                      className="mr-4 font-[530]"
+                      style={{ fontFamily: "Nunito Sans, sans-serif" }}
+                  >
                   Join us
                 </span>
-              </button>
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
 };
 
