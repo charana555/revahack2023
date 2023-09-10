7839c64c0f957304bea56b603e5bca57468ac741
Dhyuti Tewani <55708942+Dhyuti@users.noreply.github.com>
2023-09-10 18:30:20 +0530

    pizes section

--- src/components/Prizes/Prizes.jsx
+++ src/components/Prizes/Prizes.jsx
@@ -1,49 +1,41 @@
 import React from "react";
 import "./Prizes.css";
 
-const Prizes = () => {
+function Prizes () {
+  const headerStyle = {
+    color: "#6D5BF6",
+  };
+
+  const headerStyle1 = {
+    color: "#0CCA4A",
+  };
+
   return (
     <div>
       <section
         id="Prizes"
-        className="flex-col text-white text-3xl w-full h-screen flex items-center justify-center"
+        className="flex flex-col text-white text-3xl h-auto items-center justify-center mb-10%"
       >
-        <p className="text-8xl text-secondary">Prizes</p>
+        <p className="text-7xl" style={headerStyle}>Prizes</p>
 
-        <div className="container glass">
-          <ul id="nav">
-            <li>Bounty</li>
-            <li>Participation Perks</li>
-          </ul>
-          <hr id="h" />
-          <div className="prize-tag container">
-            <h4>Total Prize Pool of</h4>
-            <h4 className="INR">INR 5,00,00,000+</h4>
+        <div className="w-[50%] h-auto p-[10px] prize-tag">
+          <div className="container glass">
+            <hr className="hr" />
+            <p className="text-5xl" style={headerStyle1}>TOTAL PRIZE POOL OF</p>
+            <p className="text-8xl mb-[4%]">₹ 5,00,000</p>
           </div>
-          <div className="prize-1 container">
-            <div className="prize">
-              <h5>1st Prize</h5>
-              <p className="prize-amt">₹ 40,000</p>
+        </div>
+        <div className="flex justify-between -mx-4">
+          <div className="w-[30%] p-[10px] prize-tag">
+            <div className="container glass" style={{ backgroundColor: '#515151' }}>
+              <p className="text-7xl mt-10 mb-10" style={{ textAlign: 'center'}}>Winner</p>
+              <img src="./Images/prize_golden.svg" alt="Winner" className="custom-image"></img>
             </div>
           </div>
-          <div className="prizes container" id="prizes-2">
-            <div className="prize">
-              <h5 className="prize-title">2nd Prize</h5>
-              <p className="prize-amt">₹ 30,000</p>
-            </div>
-            <div className="prize" id="prize-3">
-              <h5 className="prize-title">3rd Prize</h5>
-              <p className="prize-amt">₹ 20,000</p>
-            </div>
-          </div>
-          <div className="prizes container" id="prizes-2">
-            <div className="prize prize3">
-              <h5 className="prize-title">Best Female Team</h5>
-              <p className="prize-amt">₹ 5,000</p>
-            </div>
-            <div className="prize prize3 prize3-2">
-              <h5 className="prize-title">Best Virtual Team</h5>
-              <p className="prize-amt">₹ 5,000</p>
+          <div className="w-[30%] p-[10px] prize-tag">
+            <div className="container glass" style={{ backgroundColor: '#515151' }}>
+              <p className="text-7xl mt-10 mb-10" style={{ textAlign: 'center'}}>Runner</p>
+              <img src="./Images/prize_sliv.svg" alt="Runner" className="custom-image"></img>
             </div>
           </div>
         </div>
