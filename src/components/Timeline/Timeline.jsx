9fd860b400df1b3c0c3de8551617fe7f9375ec92
Dhyuti Tewani <55708942+Dhyuti@users.noreply.github.com>
2023-09-10 13:37:26 +0530

    timeline with html css

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import "./Timeline.css";
 import timelineElements from "./timelineElements";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
+// import {
+//   VerticalTimeline,
+//   VerticalTimelineElement,
+// } from "react-vertical-timeline-component";
+// import "react-vertical-timeline-component/style.min.css";
 
 import sponsorbg from "../../assets/sponsorBg.png";
 
 function Timeline() {
-  const workIconStyles = { background: "#939AFF"};
-  const schoolIconStyles = { background: "#EC6548"};
   
+  const headerStyle = {
+    backgroundColor: "#EC6548",
+  };
+  const headerStyle1 = {
+    backgroundColor: "#0CCA4A",
+  };
+  const headerStyle2 = {
+    backgroundColor: "#6D5BF6",
+  };
   return (
     <section
       id="Timeline"
@@ -20,39 +27,68 @@ function Timeline() {
     >
       <img className="absolute z-10 mt-[-500%] sm:mt-[-450%] md:mt-[-250%] xl:mt-[-175%]" src={sponsorbg} />
       <p className="flex-col flex items-center justify-center text-7xl mb-[5%] mt-[140%] md:mt-[70%] xl:mt-[28%] font-roboto">Timeline</p>
-      <div>
-        <VerticalTimeline layout="1-column-left" lineColor="rgba(13, 15, 22, 0.1)">
-          {timelineElements.map((element) => {
-            const isWorkIcon = element.icon === "work";
-            const titleColor = isWorkIcon ? workIconStyles.background : schoolIconStyles.background;
+      
+      <div class="timeline-table">
+        <h2>Table 1</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th className="border-b border-r" style={headerStyle1}>Date</th>
+                    <th className="border-b" style={headerStyle1}>Event</th>
+                </tr>
+            </thead>
+            <tbody className="bg-gray-400 rounded-md bg-clip-padding bg-opacity-10">
+                <tr>
+                    <td className="border-r">2000</td>
+                    <td>Event 1: Something happened in 2000</td>
+                </tr>
+                <tr>
+                    <td className="border-r">2005</td>
+                    <td>Event 2: Another event in 2005</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
-            return (
-              <VerticalTimelineElement
-                key={element.key}
-                // date={element.date}
-                // dateClassName="date"
-                iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
-                icon={isWorkIcon ? (
-                  <img src="./Images/work.svg" alt="work icon" />
-                ) : (
-                  <img src="./Images/school.svg" alt="school icon" />
-                )}
-                contentStyle={{ background: 'rgba(13, 15, 22)', color: '#fff'}}
-                className="vertical-timeline-element"
-              >
-              <div className="">
-                <h1 className="vertical-timeline-element-title font-roboto" style={{ color: titleColor }}>{element.title}</h1>
-                <div className="h-[10%] w-full xl:w-[50%] xl:ml-[2%] bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10"
-                  style={{ fontFamily: "Fira Code, monospace" }}
-                >
-                  <p id="description">{element.description}</p>
-                </div>
-              </div>
-              </VerticalTimelineElement>
-            );
-          })}
-        </VerticalTimeline>
-      </div>
+    <div class="timeline-table">
+        <h2>Table 2</h2>
+        <table>
+            <thead>
+                <tr className="">
+                    <th className="border-b border-r" style={headerStyle}>Date</th>
+                    <th className="border-b" style={headerStyle}>Event</th>
+                </tr>
+            </thead>
+            <tbody className="bg-gray-400 rounded-md bg-clip-padding bg-opacity-10">
+                <tr>
+                    <td className="border-r">2010</td>
+                    <td>Event 3: A significant milestone in 2010</td>
+                </tr>
+                <tr>
+                    <td className="border-r">2015</td>
+                    <td>Event 4: More history in 2015</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="timeline-table">
+        <h2>Table 3</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th className="border-b border-r" style={headerStyle2}>Date</th>
+                    <th className="border-b" style={headerStyle2}>Event</th>
+                </tr>
+            </thead>
+            <tbody className="bg-gray-400 rounded-md bg-clip-padding bg-opacity-10">
+                <tr>
+                    <td className="border-r">2020</td>
+                    <td>Event 5: Recent event from 2020</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     </section>
   );
 }
