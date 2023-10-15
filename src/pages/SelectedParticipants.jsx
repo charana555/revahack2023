fae679df72b5635ec7279779af80ffda01c60333
Amogh <amoghpadukone@gmail.com>
2023-10-15 16:04:25 +0530

    added react outer

---
+++ src/pages/SelectedParticipants.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+{
+  /* checklist
+    [] make a simplified navbar
+    [] Add a button to go back to main page
+    [] make filter buttons to filter out tracks
+    [] make glassmorphics cards 
+    [] make it responsive
+
+
+
+*/
+}
+
+const SelectedParticipants = (props) => {
+  return (
+    <>
+      <h1>Participats page</h1>
+    </>
+  );
+};
+
+export default SelectedParticipants;
